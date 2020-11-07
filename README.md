c65f3b83cccc8490a4e9289211f169d4f2907939
Błażej Sewera <blazejok1@wp.pl>
2020-11-07 15:38:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+pre-commit-message-check-hook
+=========================
+
+A prepare-commit-msg hook for pre-commit.
+
+See also: https://github.com/pre-commit/pre-commit
+
+
+### Using with pre-commit
+
+Add this to your `.pre-commit-config.yaml`
+
+```yaml
+-   repo: https://github.com/jazzsewera/pre-commit-message-check-hook
+    rev: v0.0.1  # Use the ref you want to point at
+    hooks:
+    -   id: add-msg-issue-prefix
+```
+
+and install prepare-commit-msg hooks using
+```
+pre-commit install --hook-type prepare-commit-msg
+```
---
+++ message_check_hook/check_message.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+import re
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('commit_msg_filepath', required=True)
+    parser.add_argument('-r', '--regex', required=True)
+    args = parser.parse_args()
+
+    with open(args.commit_msg_filepath, 'r') as f:
+        commit_msg = f.read()
+
+    if re.match(args.regex, commit_msg):
+        exit(0)
+    else:
+        print('Commit message did not comply with configured regex.\n'
+              f'=> regex: \'{args.regex}\'',
+              file=sys.stderr)
+        exit(1)
+
+
+if __name__ == '__main__':
+    main()
